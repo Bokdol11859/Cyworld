a8e3c07a45d06327092ba8d90152f6ee7ac227c4
bokdol11859 <2019147551@yonsei.ac.kr>
2022-10-18 22:45:43 +0900

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
+import Sublayout from "../components/Sublayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Sublayout>
+        <Component {...pageProps} />
+      </Sublayout>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
