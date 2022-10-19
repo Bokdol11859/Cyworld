f36e393bde849215e257c86fd10187c385e085cc
bokdol11859 <2019147551@yonsei.ac.kr>
2022-10-19 23:42:39 +0900

    feat: lotto game create

---
+++ components/LottoGame.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+type Props = {};
+
+const LottoGame = (props: Props) => {
+  const [numbers, setNumbers] = useState<number[]>([]);
+
+  const randomizeNumber = () => {
+    let temp = [];
+
+    for (let i = 1; i <= 45; i++) {
+      temp.push(Number(i));
+    }
+
+    temp.sort(() => Math.random() - 0.5);
+
+    setNumbers(
+      temp.slice(0, 6).sort(function (a, b) {
+        return a - b;
+      })
+    );
+  };
+
+  useEffect(() => {
+    randomizeNumber();
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center w-[220px] h-[168px] bg-[#F6F6F6] border-[#999999] border-solid border-[1.5px] rounded-lg">
+      <Image src="/static/lotto.svg" alt="train" width={36} height={36} />
+      <p className="text-xs font-extrabold mb-2">LOTTO</p>
+      <p className="text-[9px] font-[Pretendard] text-[#999999] font-bold">
+        버튼을 누르세요.
+      </p>
+      <div className="flex gap-2 border-solid border-black border-[1.5px] rounded-lg px-3 py-[2px] my-2 bg-[#FFE400]">
+        {numbers.map((number) => (
+          <p key={number} className="font-bold text-sm">
+            {number}
+          </p>
+        ))}
+      </div>
+      <button
+        onClick={randomizeNumber}
+        className="px-2 py-[2px] border-black border-[1.5px] rounded-lg bg-white hover:bg-[#999999] text-black hover:text-white text-sm"
+      >
+        새로!
+      </button>
+    </div>
+  );
+};
+
+export default LottoGame;
--- components/WordGame.tsx
+++ components/WordGame.tsx
@@ -4,8 +4,8 @@ import React, { useRef, useState } from "react";
 type Props = {};
 
 const WordGame = (props: Props) => {
-  const [word, setWord] = useState("코드캠프");
-  const [result, setResult] = useState("결과!");
+  const [word, setWord] = useState<string>("코드캠프");
+  const [result, setResult] = useState<string>("결과!");
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
@@ -19,7 +19,7 @@ const WordGame = (props: Props) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-[220px] h-[168px] bg-[#F6F6F6] border-[#999999] border-solid">
+    <div className="flex flex-col items-center justify-center w-[220px] h-[168px] bg-[#F6F6F6] border-[#999999] border-solid border-[1.5px] rounded-lg">
       <Image src="/static/train.svg" alt="train" width={40} height={20} />
       <p className="text-xs font-[Pretendard] font-extrabold my-2">끝말잇기</p>
       <p className="text-[9px] font-[Pretendard] text-[#999999]">
@@ -35,7 +35,7 @@ const WordGame = (props: Props) => {
           placeholder="단어를 입력하세요."
           className="w-32 h-full px-1 text-[10px] font-[Pretendard] font-medium border-solid border-black border-[1.5px] rounded-md"
         />
-        <button className="px-2 bg-white h-full font-[Pretendard] text-xs border-solid border-black border-[1.5px] rounded-md">
+        <button className="px-2 bg-white hover:bg-[#999999] text-black hover:text-white h-full font-[Pretendard] text-xs border-solid border-black border-[1.5px] rounded-md">
           입력
         </button>
       </form>
