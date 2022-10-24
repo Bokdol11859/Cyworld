8c5f8c8ffe36436565d049c4a962a2ea1adf5802
bokdol11859 <2019147551@yonsei.ac.kr>
2022-10-25 00:38:07 +0900

    fix: diary type import fix

--- components/diary/Card.tsx
+++ components/diary/Card.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-import Diary from "../../pages/diary";
+import { Diary } from "../../typings";
 
 type Props = {
   diary: Diary;
